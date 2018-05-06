42f8a0057c3fa4f45171c94695efcdd89aca6825
sailboatlarry@gmail.com <sailboatlarry@gmail.com>
2018-05-06 13:30:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ function updatePlaylist(playlist, artist, song){
     playlist[artist] = 'you'
     return playlist
 };
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist['you']
+  return playlist
+}
